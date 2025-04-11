7c1f8b0eb30b0b79e168166a2d21308cba1680e7
prabhu <pdbiradar10@gmail.com>
2025-04-11 15:42:35 +0530

    setup

--- src/main/java/Test/controller/UserController.java
+++ src/main/java/Test/controller/UserController.java
@@ -5,7 +5,10 @@ import Test.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+
+@RestController
 @RequestMapping("/user")
 public class UserController {
 
